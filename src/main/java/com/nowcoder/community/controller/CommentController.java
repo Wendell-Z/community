2c53d6504e235d72b331f1256295d99d13a55797
Wayne96 <zhang.wenliang@nexuslink.cn>
2020-02-16 00:42:57 +0800

    完成显示贴子详情和评论发布

--- src/main/java/com/nowcoder/community/controller/CommentController.java
+++ src/main/java/com/nowcoder/community/controller/CommentController.java
@@ -24,7 +24,7 @@ public class CommentController {
 
     /**
      * 添加评论 因需要更改帖子中评论数量字段 所以需要传过来帖子ID 另外评论也需要对应帖子ID（前端已处理？）
-     * 帖子的类型 对应的userID 前端已一并提交 封装到comment中
+     * 帖子的类型 对应的userID 前端已一并提交 封装到comment中了
      *
      * @param discussPostId
      * @param comment
